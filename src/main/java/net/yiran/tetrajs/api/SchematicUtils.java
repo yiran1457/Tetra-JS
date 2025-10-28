53488c4f4eed00361699b4bfb59de0241241bbd8
yiran1457 <3270897489@qq.com>
2025-10-28 11:15:37 +0800

    添加一点小功能

--- src/main/java/net/yiran/tetrajs/api/SchematicUtils.java
+++ src/main/java/net/yiran/tetrajs/api/SchematicUtils.java
@@ -1,36 +1,79 @@
 package net.yiran.tetrajs.api;
 
+import dev.latvian.mods.kubejs.typings.Info;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.ToolAction;
 import se.mickelus.tetra.TetraToolActions;
-import se.mickelus.tetra.module.schematic.SchematicRarity;
-import se.mickelus.tetra.module.schematic.SchematicType;
-import se.mickelus.tetra.module.schematic.UpgradeSchematic;
+import se.mickelus.tetra.module.schematic.*;
 
 public class SchematicUtils {
     public static SchematicUtils INSTANCE = new SchematicUtils();
 
+    @Info("获取原理图的经验消耗")
     public int getExperienceCost(UpgradeSchematic schematic, ItemStack targetStack, ItemStack[] materials, String slot) {
         return schematic.getExperienceCost(targetStack, materials, slot);
     }
 
+    @Info("获取原理图所需的锤等级")
     public int getRequiredHammerLevel(UpgradeSchematic schematic, ItemStack targetStack, ItemStack[] materials) {
         return getRequiredToolLevel(schematic, targetStack, materials, TetraToolActions.hammer);
     }
 
+    @Info("获取原理图所需的指定锤等级")
     public int getRequiredToolLevel(UpgradeSchematic schematic, ItemStack targetStack, ItemStack[] materials, ToolAction toolAction) {
         return schematic.getRequiredToolLevel(targetStack, materials, toolAction);
     }
 
+    @Info("获取最高的工具等级需求")
+    public int getMaxRequiredToolLevel(UpgradeSchematic schematic, ItemStack targetStack, ItemStack[] materials) {
+        int max = 0;
+        for (int value : schematic.getRequiredToolLevels(targetStack, materials).values()) {
+            max = Math.max(max, value);
+        }
+        return max;
+    }
+
+    @Info("获取全部工具等级需求总和")
+    public int getTotalRequiredToolLevel(UpgradeSchematic schematic, ItemStack targetStack, ItemStack[] materials) {
+        int total = 0;
+        for (int value : schematic.getRequiredToolLevels(targetStack, materials).values()) {
+            total += value;
+        }
+        return total;
+    }
+
+    @Info("获取原理图的类型(other,improvement,minor,major)")
     public SchematicType getType(UpgradeSchematic schematic) {
         return schematic.getType();
     }
 
+    @Info("获取原理图的稀有度(hone,temporary,basic")
     public SchematicRarity getRarity(UpgradeSchematic schematic) {
         return schematic.getRarity();
     }
 
+    @Info("判断此原理图是否会替换部件(应该是)")
     public boolean willReplaced(UpgradeSchematic schematic, ItemStack targetStack, ItemStack[] materials, String slot) {
         return schematic.willReplace(targetStack, materials, slot);
+    }
+
+    @Info("判断此原理图是否是打磨")
+    public boolean isHoning(UpgradeSchematic schematic) {
+        return schematic.isHoning();
+    }
+
+    @Info("判断此原理图是否为ConfigSchematic(一般你数据包生成的就是这类)")
+    public boolean isConfigSchematic(UpgradeSchematic schematic) {
+        return schematic instanceof ConfigSchematic;
+    }
+
+    @Info("判断此原理图是否是修复(修补耐久的那个)")
+    public boolean isRepairSchematic(UpgradeSchematic schematic) {
+        return schematic instanceof RepairSchematic;
+    }
+
+    @Info("判断此原理图是否是附魔原理图(消耗附魔书附魔的)")
+    public boolean isEnchantSchematic(UpgradeSchematic schematic) {
+        return schematic instanceof BookEnchantSchematic;
     }
 }
--- src/main/java/net/yiran/tetrajs/api/TetraJSUtils.java
+++ src/main/java/net/yiran/tetrajs/api/TetraJSUtils.java
@@ -1,5 +1,6 @@
 package net.yiran.tetrajs.api;
 
+import dev.latvian.mods.kubejs.typings.Info;
 import net.minecraft.world.item.ItemStack;
 import se.mickelus.tetra.aspect.ItemAspect;
 import se.mickelus.tetra.data.DataManager;
@@ -14,6 +15,7 @@ import java.util.Set;
 public class TetraJSUtils {
     public static TetraJSUtils INSTANCE = new TetraJSUtils();
 
+    @Info("检测一个物品是否为tetra的材料")
     public boolean stackIsTetraMaterial(ItemStack stack) {
         return DataManager.instance.materialData
                 .getData()
@@ -25,6 +27,7 @@ public class TetraJSUtils {
                 .anyMatch(itemPredicate -> itemPredicate.matches(stack));
     }
 
+    @Info("获取改进等级,不存在则返回-1")
     public int getImprovementLevel(ItemStack stack, String slot, String improvement) {
         if (stack.getItem() instanceof IModularItem modularItem) {
             if (modularItem.getModuleFromSlot(stack, slot) instanceof ItemModuleMajor moduleMajor) {
@@ -34,12 +37,26 @@ public class TetraJSUtils {
         return -1;
     }
 
-    public void addImprovementLevel(ItemStack stack, String slot, String improvement, int level) {
+    @Info("给物品添加改进")
+    public void addImprovement(ItemStack stack, String slot, String improvement, int level) {
         if (stack.getItem() instanceof IModularItem modularItem) {
             if (modularItem.getModuleFromSlot(stack, slot) instanceof ItemModuleMajor moduleMajor) {
                 moduleMajor.addImprovement(stack, improvement, level);
             }
         }
+    }
+
+    @Info("给物品添加改进,并检查添加的是否为更高级的改进,如果不是则取消添加，成功添加返回true")
+    public boolean addCheckedImprovement(ItemStack stack, String slot, String improvement, int level) {
+        if (stack.getItem() instanceof IModularItem modularItem) {
+            if (modularItem.getModuleFromSlot(stack, slot) instanceof ItemModuleMajor moduleMajor) {
+                if (level > moduleMajor.getImprovementLevel(stack, improvement)) {
+                    moduleMajor.addImprovement(stack, improvement, level);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public int getAspectLevel(ItemStack stack, String slot, ItemAspect aspect) {
@@ -66,7 +83,7 @@ public class TetraJSUtils {
         if (stack.getItem() instanceof IModularItem modularItem) {
             ItemModule itemModule = modularItem.getModuleFromSlot(stack, slot);
             if (itemModule != null) {
-                return itemModule.hasAspect(stack,aspect);
+                return itemModule.hasAspect(stack, aspect);
             }
         }
         return false;
